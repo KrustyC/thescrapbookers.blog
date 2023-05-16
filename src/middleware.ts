a103be828c3e9262af70df90a2905744838b6798
Davide Crestini <davide.crestini94@gmail.com>
2023-05-16 19:23:28 +0700

    chore: trying to sort out middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import createMiddleware from "next-intl/middleware";
+// import { NextRequest, NextResponse } from "next/server";
 
 export default createMiddleware({
   locales: ["en", "it"],
@@ -7,9 +8,37 @@ export default createMiddleware({
   defaultLocale: "en",
 });
 
+// const intlMiddleware = createMiddleware({
+//   locales: ["en", "it"],
+//   defaultLocale: "en",
+// });
+
+// const SKIP_PREFIXES = [
+//   "/_next",
+//   "/api",
+//   "/en/api",
+//   "/it/api",
+//   "/favicon.ico",
+//   "/robots.txt",
+//   "/_ipx",
+//   "/__nextjs",
+//   "/icons",
+// ];
+
+// export function middleware(req: NextRequest) {
+//   if (SKIP_PREFIXES.some((prefix) => req.nextUrl.pathname.startsWith(prefix))) {
+//     return NextResponse.next();
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export default intlMiddleware;
+
 export const config = {
   // Skip all paths that should not be internationalized
-  matcher: [
-    "/((?!icons|_next/static|_next/image|sitemap.xml|robots.txt|_ipx|favicon.ico).*)",
-  ],
+  matcher:
+    "/((?!icons|_next|__nexts|sitemap.xml|robots.txt|_ipx|favicon.ico|api|en/api|it/api).*)",
 };
+
+// |api|en/api|it/api
