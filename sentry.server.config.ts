d12df999a934d9864894ff81b93548050f1784ef
Davide Crestini <davide.crestini94@gmail.com>
2023-05-18 23:25:24 +0700

    chore: disabled profiling for the time being due to an issue with the SDK

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,8 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { ProfilingIntegration } from "@sentry/profiling-node";
+// import { ProfilingIntegration } from "@sentry/profiling-node";
+// import '@sentry/tracing';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -9,9 +10,9 @@ Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1,
   debug: false,
-  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
-  integrations: [
-    // Add profiling integration to list of integrations
-    new ProfilingIntegration(),
-  ],
+//   profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+//   integrations: [
+//     // Add profiling integration to list of integrations
+//     new ProfilingIntegration(),
+//   ],
 });
