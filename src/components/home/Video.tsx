77df60676216d24ef26193c6ff2d440d7f6cb4b2
Davide Crestini <davide.crestini94@gmail.com>
2023-05-23 22:44:54 +0700

    feat: added new video playbackId

--- src/components/home/Video.tsx
+++ src/components/home/Video.tsx
@@ -14,7 +14,7 @@ export const Video: React.FC = () => {
       defaultShowRemainingTime={false}
       className="w-full bg-gray-200 -mb-2"
       streamType="on-demand"
-      playbackId="nxCCwFBmmiMRxKFPlOMOtkYea29hWEHgSDbj02QfvNC4"
+      playbackId="tOV00Jf00oWm2Nw02tq3OAVaUa6Gv8xUQ3mkmuWVyiizDI"
       metadata={{
         video_title: "A video of Ban Sen",
       }}
