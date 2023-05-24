5c61bee9cc0c4513bd57dbd23cbdbfd40dbb4aab
Davide Crestini <davide.crestini94@gmail.com>
2023-05-24 22:35:34 +0700

    fix: responsive UI of AboutUs section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -100,7 +100,7 @@ export default function Home({ params }: { params: { locale: AppLocale } }) {
 
         {/* <PhotoDumpSection /> */}
 
-        {/* <AboutUsSection /> */}
+        <AboutUsSection />
 
         <HighlightSection />
 
