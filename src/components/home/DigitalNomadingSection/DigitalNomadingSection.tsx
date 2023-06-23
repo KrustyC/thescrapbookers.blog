5afdc61c0e86ab2877fe9be213cfe028ce12a526
Davide Crestini <davide.crestini94@gmail.com>
2023-06-23 18:36:25 +0800

    fix: removed useless translations in digital nomading section

--- src/components/home/DigitalNomadingSection/DigitalNomadingSection.tsx
+++ src/components/home/DigitalNomadingSection/DigitalNomadingSection.tsx
@@ -1,9 +1,5 @@
-import { getTranslations } from "next-intl/server";
-
 import { getPostDigitalNomadingHighlightedPost } from "@/api";
 import type { AppLocale } from "@/types/global";
-
-import { SectionWithTitle } from "../SectionWIthTitle/SectionWithTitle";
 
 import { DigitalNomadingSectionSkeleton } from "./DigitalNomadingSectionSkeleton";
 import { SinglePost } from "./SinglePost";
@@ -13,8 +9,6 @@ export default async function DigitalNomadingSection({
 }: {
   locale: AppLocale;
 }) {
-  const t = await getTranslations("Home.DigitalNomading");
-
   try {
     const { post } = await getPostDigitalNomadingHighlightedPost({ locale });
 
