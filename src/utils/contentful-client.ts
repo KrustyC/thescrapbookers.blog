f26c1d2a357a7ab3bae63c1db3cb2a7a7dcdb594
Davide Crestini <davide.crestini94@gmail.com>
2023-07-11 16:24:12 +0800

    fix: small fix

--- src/utils/contentful-client.ts
+++ src/utils/contentful-client.ts
@@ -3,12 +3,12 @@ import { type ContentfulClientApi, createClient } from "contentful";
 let cachedClient: ContentfulClientApi<undefined> | undefined;
 
 export function getContentfulClient(): ContentfulClientApi<undefined> {
-  // if (!cachedClient) {
-  return createClient({
-    space: process.env.CONTENTFUL_SPACE_ID as string,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
-  });
-  // }
+  if (!cachedClient) {
+    return createClient({
+      space: process.env.CONTENTFUL_SPACE_ID as string,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+    });
+  }
 
-  // return cachedClient;
+  return cachedClient;
 }
