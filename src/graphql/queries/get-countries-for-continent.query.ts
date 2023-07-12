a149d094b1841fd3b1e6a5f05e15a12a6fd28693
Davide Crestini <davide.crestini94@gmail.com>
2023-07-12 16:54:09 +0800

    fix: countries ordering

--- src/graphql/queries/get-countries-for-continent.query.ts
+++ src/graphql/queries/get-countries-for-continent.query.ts
@@ -35,6 +35,12 @@ interface GetCountriesForContinentResponse {
   countries: Array<Pick<Country, "name" | "slug">>;
 }
 
+/**
+ * This is a small hack to quickly sort countries according to the order I prefer.
+ * in the future it may be done directly on the API, or Contentful.
+ */
+const COUNTRY_ORDERED_SLUGS = ["thailand", "laos", "vietnam", "cambodia"];
+
 export async function getCountriesForContinent({
   continentSlug,
   locale,
@@ -53,8 +59,21 @@ export async function getCountriesForContinent({
       },
     });
 
+    const countries = data.data.countryCollection.items;
+
+    // Filter out countries that don't have a slug
+    const filteredCountries = countries.filter(country => !!country.slug)
+    filteredCountries.sort((a, b) => {
+      if (!a.slug || !b.slug) return 0;
+
+      return (
+        COUNTRY_ORDERED_SLUGS.indexOf(a.slug) -
+        COUNTRY_ORDERED_SLUGS.indexOf(b.slug)
+      );
+    });
+
     return {
-      countries: data.data.countryCollection.items,
+      countries: filteredCountries,
     };
   } catch (error) {
     console.error(error);
