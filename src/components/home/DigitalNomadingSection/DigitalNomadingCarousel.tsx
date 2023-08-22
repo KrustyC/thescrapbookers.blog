6524f7986051da0f5fd369a88c88e1a0f888768e
Davide Crestini <davide.crestini94@gmail.com>
2023-08-22 22:27:35 +0800

    fix: digital nomad section

--- src/components/home/DigitalNomadingSection/DigitalNomadingCarousel.tsx
+++ src/components/home/DigitalNomadingSection/DigitalNomadingCarousel.tsx
@@ -14,11 +14,11 @@ interface PostProps {
 
 export const DigitalNomadingCarousel: React.FC<PostProps> = ({ posts }) => {
   return (
-    <div className="w-full flex flex-row gap-4 lg:gap-8">
+    <div className="w-full flex flex-col lg:flex-row gap-6 lg:gap-8">
       {posts.map((post) => (
         <Link key={post.href} href={post.href || "/"} className="flex-1">
           <div className="flex flex-col gap-4">
-            <div className="w-full h-[250px] lg:h-[300px] xl:h-[350px] relative rounded-xl drop-shadow-xl">
+            <div className="w-full h-[100px] lg:h-[300px] xl:h-[350px] relative rounded-xl drop-shadow-xl">
               <Image
                 fill
                 className="rounded-xl"
