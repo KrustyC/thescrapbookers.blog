d59c7c1011724d499a6fd0b2a1b8666fec95a0bf
Davide Crestini <davide.crestini94@gmail.com>
2023-12-18 10:18:55 +0100

    feat: started code for exhibitions

---
+++ src/graphql/queries/get-exhibition.query.ts
@@ -0,0 +1,146 @@
+import { gql } from "@apollo/client";
+import { getApolloServerClient } from "src/graphql/apollo-server-client";
+
+import { Post as PostGraphQL } from "@/types/generated/graphql";
+import { AppLocale, Post } from "@/types/global";
+
+import { parseGraphQLNextPost, parseGraphQLPost } from "../parsers/posts";
+
+interface GetPostParams {
+  slug: string;
+  locale: AppLocale;
+  isPreview?: boolean;
+}
+
+interface PostQueryResposne {
+  postCollection: {
+    items: PostGraphQL[];
+  };
+}
+
+interface GetPostResponse {
+  post: Post;
+  nextPost?: Pick<Post, "title" | "slug" | "date" | "mainImage" | "smallIntro">;
+}
+
+const GET_POST_QUERY = gql`
+  query ($slug: String!, $locale: String!, $preview: Boolean!) {
+    postCollection(
+      where: { slug: $slug }
+      preview: $preview
+      locale: $locale
+      limit: 1
+    ) {
+      items {
+        title
+        metaTitle
+        metaDescription
+        slug
+        smallIntro
+        date
+        category
+        richtext {
+          json
+          links {
+            entries {
+              block {
+                sys {
+                  id
+                }
+                ... on Video {
+                  video
+                  name
+                  description
+                }
+              }
+            }
+            assets {
+              block {
+                sys {
+                  id
+                }
+                contentType
+                title
+                description
+                width
+                height
+                url
+              }
+            }
+          }
+        }
+        author {
+          name
+        }
+        country {
+          name
+          slug
+          continent {
+            name
+            slug
+          }
+        }
+        mainImage {
+          title
+          description
+          width
+          height
+          url
+        }
+        thumbnailImage {
+          title
+          description
+          width
+          height
+          url
+        }
+        nextPost {
+          title
+          slug
+          smallIntro
+          date
+          mainImage {
+            title
+            description
+            width
+            height
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+export async function getPost({
+  slug,
+  locale,
+  isPreview = false,
+}: GetPostParams): Promise<GetPostResponse> {
+  try {
+    const data = await getApolloServerClient({
+      isPreview,
+    }).query<PostQueryResposne>({
+      query: GET_POST_QUERY,
+      variables: { slug, locale, preview: isPreview },
+      context: {
+        fetchOptions: {
+          next: {
+            revalidate:
+              isPreview || process.env.DISABLE_CACHE === "true" ? 0 : 3600,
+          },
+        },
+      },
+    });
+
+    const post = data.data.postCollection.items[0];
+
+    return {
+      post: parseGraphQLPost(post),
+      nextPost: post.nextPost ? parseGraphQLNextPost(post.nextPost) : undefined,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch post");
+  }
+}
