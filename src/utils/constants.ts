c5c9c0d8883b42c45483099a04fc8ae7d9fedb4b
Davide Crestini <davide.crestini94@gmail.com>
2023-12-24 17:35:31 +0100

    fix: order of posts in digital nomading section on homepage

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,5 +13,5 @@ export const COUNTRY_ORDERED_SLUGS = [
 export const DIGITAL_NOMADING_POSTS_ORDERED_SLUGS = [
   "the-digital-nomad-transition",
   "what-is-a-digital-nomad",
-  "coworking-spaces-vs-cafes-the-ultimate-guide-for-digital-nomads",
+  "co-working-spaces-vs-cafes-the-ultimate-guide-for-digital-nomads",
 ];
